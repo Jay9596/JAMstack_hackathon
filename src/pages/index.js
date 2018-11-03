42aa68751862f0a4e2fd6965641afc2c9cdc0adc
Jay Shah <Jay9596@users.noreply.github.com>
2018-11-04 02:34:13 +0530

    Index changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,48 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'gatsby'
+import axios from 'axios'
 
 import Layout from '../components/layout'
 import Image from '../components/image'
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+class IndexPage extends Component {
+  constructor() {
+    super()
+    this.setState = {
+      user: {},
+    }
+  }
 
+  componentDidMount() {
+    axios
+      // .get('https://api.github.com/users/jay9596')
+      .get('.netlify/function/info')
+      .then(res => {
+        let data = JSON.parse(res)
+        console.log(data)
+        this.setState({
+          user: {
+            name: data.name,
+            avatar: data.avatar,
+            followers: data.followers,
+            following: data.following,
+          },
+        })
+      })
+      .catch(err => {
+        // callback(err, null)
+        console.error(err)
+      })
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.state.user.map(item => (
+          <li>{item}</li>
+        ))}
+      </ul>
+    )
+  }
+}
 export default IndexPage
